6809886a42590b2fe0e6b6cab787ff49eac4e20f
AdvilCaps <38378405+AdvilCaps@users.noreply.github.com>
2018-05-01 20:12:48 +0200

    deleteUser

--- Bankrupt/src/com/bankrupt/tools/Values.java
+++ Bankrupt/src/com/bankrupt/tools/Values.java
@@ -3,7 +3,7 @@ package com.bankrupt.tools;
 public final class Values {
 	public static final int customerRole = 1;
 	public static final int bankerRole = 2;
-	public static final int savingAccountType=0;
-	public static final int currentAccountType=1;
+	public static final int savingAccountType=1;
+	public static final int currentAccountType=2;
 	public static final String currentAccount="Courant";
 }
