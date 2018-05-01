156a1b6b4eba3aaa33e2adf41c29f3a572a73a9e
AdvilCaps <38378405+AdvilCaps@users.noreply.github.com>
2018-05-01 20:55:22 +0200

    ajout methode dans interface operationmangement

--- Bankrupt/src/com/bankrupt/tools/OperationManagement.java
+++ Bankrupt/src/com/bankrupt/tools/OperationManagement.java
@@ -1,4 +1,6 @@
 package com.bankrupt.tools;
+import java.util.Vector;
+
 import com.bankrupt.bankaccount.*;
 import com.bankrupt.datatools.SQLInteraction;
 import com.bankrupt.user.User;
@@ -6,4 +8,5 @@ import com.bankrupt.user.User;
 public interface OperationManagement {
 	public boolean makeOperation(User currentUser, double amount, int numberAccountSender, int numberAccountReceiver, SQLInteraction sqlInteraction);
 	public BankAccount findBankAccount(int customerID, int accountType, int savingType ,SQLInteraction sqlInteraction);
+	public Vector<Operation> getOperationsOfAccount(int accountId,SQLInteraction sqlInteraction);
 }
