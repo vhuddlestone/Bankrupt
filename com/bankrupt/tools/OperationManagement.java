3e08f50836e6c810302bdd9cbff5e981a510c9de
vhe <valentin.huddlestone95@orange.fr>
2018-05-02 12:38:37 +0200

    ménage

--- com/bankrupt/tools/OperationManagement.java
+++
@@ -1,9 +0,0 @@
-package com.bankrupt.tools;
-import com.bankrupt.bankaccount.*;
-import com.bankrupt.datatools.SQLInteraction;
-import com.bankrupt.user.User;
-
-public interface OperationManagement {
-	public boolean makeOperation(User currentUser, double amount, int numberAccountSender, int numberAccountReceiver, SQLInteraction sqlInteraction);
-	public BankAccount findBankAccount(int customerID, int accountType, int savingType ,SQLInteraction sqlInteraction);
-}
